28b9270ddf44f3a72ea022f7705e5680d303fd51
kurihara <kurihara@web-ad.co.jp>
2022-04-12 10:03:57 +0900

    Initial Commit

---
+++ Instagram/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Instagram
+//
+//  Created by kurihara on 2022/04/12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
