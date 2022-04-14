18da736de41b74ca3044f3f44c18c355fb1252f3
kurihara <kurihara@web-ad.co.jp>
2022-04-14 12:37:55 +0900

    投稿部分まで完了

---
+++ Instagram/Const.swift
@@ -0,0 +1,13 @@
+//
+//  Const.swift
+//  Instagram
+//
+//  Created by kurihara on 2022/04/14.
+//
+
+import Foundation
+
+struct Const {
+    static let ImagePath = "images"
+    static let PostPath = "posts"
+}
