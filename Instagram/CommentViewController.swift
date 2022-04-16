d4d79fadff53a63606754477e7d37f64f16ad824
kurihara <kurihara@web-ad.co.jp>
2022-04-16 20:54:00 +0900

    コメント投稿用の画面

---
+++ Instagram/CommentViewController.swift
@@ -0,0 +1,17 @@
+//
+//  CommentViewController.swift
+//  Instagram
+//
+//  Created by kurihara on 2022/04/16.
+//
+
+import UIKit
+
+class CommentiewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+}
